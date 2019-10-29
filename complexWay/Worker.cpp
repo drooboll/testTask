ab1e428a15f7c1f37e21d07b2d4a42e742e6c491
drooboll <drobotun.mail@gmail.com>
2019-10-29 14:09:57 +0700

    Worker fix

--- complexWay/Worker.cpp
+++ complexWay/Worker.cpp
@@ -6,13 +6,12 @@ Worker::Worker(std::shared_ptr<ConsoleParser> cp, std::shared_ptr<Options> op) {
 }
 
 bool Worker::work() {
-    std::cout << "Wrong or incomplete set of flags" << std::endl;
-    std::cout << "Run " << cp->getProgramName() << " with " <<
-              op->flags[Options::flagType::HELP_FLAG] << " or " << op->flags[Options::flagType::HELP_LONG_FLAG] <<
-              " to see help." << std::endl;
     return true;
 }
 
 void Worker::printResult() {
-    throw std::runtime_error("No arguments specified.");
+    std::cout << "Wrong or incomplete set of flags" << std::endl;
+    std::cout << "Run " << cp->getProgramName() << " with " <<
+    op->flags[Options::flagType::HELP_FLAG] << " or " << op->flags[Options::flagType::HELP_LONG_FLAG] <<
+    " to see help." << std::endl;
 }
